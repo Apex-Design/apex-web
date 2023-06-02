14530e53b6a9f9741ed5635833a567e67eecc570
Gabriel Beal <gabebeal725@gmail.com>
2023-06-02 10:44:38 -0700

    logo fix 1.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -162,7 +162,7 @@
 </script>
 
 <header class="flex flex-row px-10 sm:px-14 md:px-16 lg:px-20 mx-auto mt-8 md:mt-16">
-	<img src="/apex.svg" alt="Apex Design" class="w-14 mt-4 z-10" />
+	<img src="./apex.svg" alt="Apex Design" class="w-14 mt-4 z-10" />
 	<nav class="hidden md:flex flex-row items-start text-sm ml-auto">
 		{#each NAV_ITEMS as item, index}
 			<a
